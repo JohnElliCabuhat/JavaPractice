07b89f7a8e0b0137aa5271c0343dcc8a186ec7fc
John Elli Cabuhat <johnelli.cabuhat@gmail.com>
2026-06-01 18:11:04 +0800

    added variables and display them

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int number = 28;
+        String name = "Elli";
+        System.out.println(name);
+        System.out.println(number);
+        
     }
 }

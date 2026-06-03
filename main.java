1d0714371d4eba7bb13aa1fdab87bb4d38a8f714
John Elli Cabuhat <johnelli.cabuhat@gmail.com>
2026-06-03 14:26:41 +0800

    Started calculator project; added main operators

--- main.java
+++ main.java
@@ -1,14 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        boolean isFun = true;
-        char dollar = '$';
-        boolean sad = false;
-        String fullName = "John Doe";
+        double a = 5;
+        double b = 3;
+        double sum = a + b;
+        double difference = a - b;
+        double product = a * b;
+        double quotient = a / b;
 
-
-        System.out.println("isFun = " + isFun);
-        System.out.println("dollar = " + dollar);
-        System.out.println("sad = " + sad);
-        System.out.println("fullName = " + fullName);
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
     }
 }
